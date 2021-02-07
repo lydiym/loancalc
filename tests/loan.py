9fca044b69ea26d08ce20fa1e6fd166ba4a8e30d
lydiym <spacefuryphonk@gmail.com>
2021-02-07 20:37:05 +0300

    remove excess test

--- tests/loan.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-from loancalc import Loan
-
-
-def test_loan():
-    loan = Loan(body=1000000, percentage=10.7, time=120, initial_date=date(2020, 12, 1))
-    print(loan)
